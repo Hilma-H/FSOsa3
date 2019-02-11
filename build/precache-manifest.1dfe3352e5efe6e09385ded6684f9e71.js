7929a8385c3b28556a8af720b011a011796735e1
hilma <hilma.holappa@helsinki.fi>
2019-02-11 16:52:58 +0200

    internetversio t3.11

---
+++ build/precache-manifest.1dfe3352e5efe6e09385ded6684f9e71.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c809fab2fc826b28b43c",
+    "url": "/static/js/main.c809fab2.chunk.js"
+  },
+  {
+    "revision": "5a4a443bcbbb507de4c2",
+    "url": "/static/js/1.5a4a443b.chunk.js"
+  },
+  {
+    "revision": "cdbf4f1b9fc8b26580c577e4e689949a",
+    "url": "/index.html"
+  }
+];
---
+++ build/precache-manifest.e2a1f279fe19a809d41c5df267391100.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b7f01430f2e6354cfef0",
+    "url": "/static/js/main.b7f01430.chunk.js"
+  },
+  {
+    "revision": "5a4a443bcbbb507de4c2",
+    "url": "/static/js/1.5a4a443b.chunk.js"
+  },
+  {
+    "revision": "b90a90660067adda6c8cdd4e560e18c3",
+    "url": "/index.html"
+  }
+];
