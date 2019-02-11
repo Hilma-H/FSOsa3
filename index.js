1a18e973401aaf65ad1bcd4dc0d8d9611d5d9104
hilma <hilma.holappa@helsinki.fi>
2019-02-11 12:26:29 +0200

    Tehtävät 3.4 asti

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
@@ -35,6 +38,38 @@ app.get('/', (req, res) => {
 app.get('/api/persons', (req, res) => {
     res.json(persons)
   })
+
+app.get('/info', (req, res) => {
+    const x = persons.length
+    console.log(Date)
+    const date = Date()
+    res.json(`Puhelinluettelossa on ${x} henkilöä ${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(p => p.id !== id);
+
+  response.status(204).end();
+})
+
+app.post('/api/persons', (request, response) => {
+  const person = request.body
+  console.log(person)
+
+  response.json(person)
+})
 
 const port = 3001
 app.listen(port)
